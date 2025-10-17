e828cf9c72e3511a9aa589a07b79ee47d5884fcb
Curro-H <franhu@gmail.com>
2025-10-17 15:48:43 +0200

    Update Parse initialization with new keys

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -1,6 +1,6 @@
 import Parse from 'parse';
 
-Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
+Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'Vfn7YufxGzsYfCx7t8W9t1GUqzwWg5Chz3dFeuqr');
 Parse.serverURL = 'https://parseapi.back4app.com';
 
 export default Parse;

d0617b42fa1af5ecef5e14e6d33f21edd33a2b10
Curro-H <curro.h@example.com>
2025-10-17 12:59:43 +0000

    fix: usar parse.esm.js para compatibilidad Vite/Pages

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -1,7 +1,6 @@
-import * as Parse from 'parse/dist/parse.min.js';
+import Parse from 'parse/dist/parse.esm.js';
 
-Parse.default.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
-Parse.default.serverURL = 'https://parseapi.back4app.com';
+Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
+Parse.serverURL = 'https://parseapi.back4app.com';
 
-export default Parse.default;
-
+export default Parse;

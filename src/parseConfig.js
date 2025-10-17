e30e64963dbd3f9a728e30b53c88111fd4b630d5
Curro-H <franhu@gmail.comH>
2025-10-17 16:38:21 +0200

    empty commit

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -3,4 +3,5 @@ import Parse from 'parse';
 Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'Vfn7YufxGzsYfCx7t8W9t1GUqzwWg5Chz3dFeuqr');
 Parse.serverURL = 'https://parseapi.back4app.com';
 
+
 export default Parse;

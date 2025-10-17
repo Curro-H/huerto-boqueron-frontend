14f4a438ba1ef68448c6cf34a8e6a6b8167f565b
Curro-H <curro.h@example.com>
2025-10-17 13:01:21 +0000

    fix: import Parse clásico tras reinstalar parse@4

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -1,4 +1,4 @@
-import Parse from 'parse/dist/parse.esm.js';
+import Parse from 'parse';
 
 Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
 Parse.serverURL = 'https://parseapi.back4app.com';

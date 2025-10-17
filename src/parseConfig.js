d42d806eb58d1f761317208126222c6c9e8c5360
Curro-H <curro.h@example.com>
2025-10-17 12:55:05 +0000

    fix: Parse import compatible con Vite y static hosting

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -1,6 +1,7 @@
-import Parse from 'parse/dist/parse.min.js';
+import * as Parse from 'parse/dist/parse.min.js';
 
-Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
-Parse.serverURL = 'https://parseapi.back4app.com';
+Parse.default.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
+Parse.default.serverURL = 'https://parseapi.back4app.com';
 
-export default Parse;
+export default Parse.default;
+

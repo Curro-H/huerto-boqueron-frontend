01e65cba38da31182fffc2452e1c96526070b02c
Curro-H <curro.h@example.com>
2025-10-17 12:51:28 +0000

    fix: importar Parse como parse/dist/parse.min.js para Vite

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -1,4 +1,4 @@
-import Parse from 'parse';
+import Parse from 'parse/dist/parse.min.js';
 
 Parse.initialize('DVZZ0duSaOnAlzRFxY8QfKefIpouP4iaNg51oKno', 'I1UjxSkRoE1Fe7pacb3rYU6AiOs9DGyiLk81kMrg');
 Parse.serverURL = 'https://parseapi.back4app.com';
